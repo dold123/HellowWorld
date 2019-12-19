354c966ff769599ef997cc976369090a95609d94
User <User@YD01-19>
2019-12-19 12:46:22 +0900

    sdf

--- HellowWorld/src/Operators/Formuliti.java
+++ HellowWorld/src/Operators/Formuliti.java
@@ -2,6 +2,7 @@ package Operators;
 
 public class Formuliti {
 	public static void main(String[] args) {
+		
 		for (int i = 1; i <= 9; i++) {
 			for (int j = 1; j <= i; j++) {
 				System.out.print("*");
